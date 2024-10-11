85e41194bfd07cd0f564c1b2c9531987de0a3112
UT01397 <UT01397@UNICOMTIC8>
2024-10-11 16:22:24 +0530

    Add project files.

---
+++ BookRentalManagementSystem_V2/book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookRentalManagementSystem_V2
+{
+    internal class book
+    {
+        string bookId {  get; set; }
+         string title { get; set; }
+         string autor { get; set; }
+
+        decimal renralprice { get; set; }
+
+
+    }
+}
